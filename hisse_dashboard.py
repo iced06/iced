47ef80b83d317e6b28b3fa16c593ce60c8a22418
iced06 <140918822+iced06@users.noreply.github.com>
2025-02-05 22:29:07 +0300

    Update hisse_dashboard.py

--- hisse_dashboard.py
+++ hisse_dashboard.py
@@ -26,8 +26,8 @@ from tqdm import tqdm
 import mplfinance as mpf
 import plotly.graph_objects as go
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 import dash_auth
 import plotly.subplots as ms
 
